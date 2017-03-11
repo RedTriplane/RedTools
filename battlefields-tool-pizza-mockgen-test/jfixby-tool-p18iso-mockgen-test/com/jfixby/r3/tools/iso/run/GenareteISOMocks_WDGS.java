cd488d9df365b34f7b9412506ba298f79eb5a223
J Fixby <github@jfixby.com>
2017-03-11 21:12:17 +0100

    ...

--- battlefields-tool-pizza-mockgen-test/jfixby-tool-p18iso-mockgen-test/com/jfixby/r3/tools/iso/run/GenareteISOMocks_WDGS.java
+++ battlefields-tool-pizza-mockgen-test/jfixby-tool-p18iso-mockgen-test/com/jfixby/r3/tools/iso/run/GenareteISOMocks_WDGS.java
@@ -5,8 +5,11 @@ import java.io.IOException;
 
 import com.jfixby.examples.wdgs.WDGS_P18_Palette;
 import com.jfixby.examples.wdgs.WDGS_Pizza_Palette;
+import com.jfixby.r3.ext.api.patch18.P18;
 import com.jfixby.r3.ext.api.scene2d.srlz.Scene2DPackage;
 import com.jfixby.r3.ext.api.scene2d.srlz.SceneStructure;
+import com.jfixby.r3.ext.p18t.red.RedP18Terrain;
+import com.jfixby.r3.ext.red.terrain.RedTerrain;
 import com.jfixby.r3.tools.api.iso.GeneratorParams;
 import com.jfixby.r3.tools.api.iso.IsoMockPaletteGenerator;
 import com.jfixby.r3.tools.api.iso.IsoMockPaletteResult;
@@ -30,11 +33,20 @@ import com.jfixby.tools.gdx.texturepacker.api.TexturePacker;
 import com.jfixby.tools.gdx.texturepacker.api.TexturePackingSpecs;
 import com.jfixby.util.iso.api.Isometry;
 import com.jfixby.util.iso.red.RedIsometry;
+import com.jfixby.util.p18t.api.P18Terrain;
+import com.jfixby.util.patch18.red.RedP18;
+import com.jfixby.util.terain.test.api.palette.Terrain;
+import com.jfixby.utl.pizza.api.Pizza;
+import com.jfixby.utl.pizza.red.RedPizza;
 
 public class GenareteISOMocks_WDGS {
 
 	public static void main (final String[] args) throws IOException {
 		ScarabeiDesktop.deploy();
+		P18Terrain.installComponent(new RedP18Terrain());
+		P18.installComponent(new RedP18());
+		Terrain.installComponent(new RedTerrain());
+		Pizza.installComponent(new RedPizza());
 
 		Isometry.installComponent(new RedIsometry());
 		// IsoMockPaletteGenerator
